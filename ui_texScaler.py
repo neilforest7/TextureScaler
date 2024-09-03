874eff4ddb521605cb883b9503ba6ec974885e0c
neilforest7 <markeloffack@gmail.com>
2024-09-04 05:48:23 +0800

    Tidy project

--- ui_texScaler.py
+++ ui_texScaler.py
@@ -12,7 +12,7 @@ from PySide6.QtCore import *
 from PySide6.QtGui import *
 from PySide6.QtWidgets import *
 
-import icons_new_rc
+import resources.icons_new_rc as icons_new_rc
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
